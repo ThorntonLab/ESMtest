9706b9f4bc08b4196c02aa4ac71ed5005d564d50
Kevin R Thornton <krthornt@thornton01.bio.uci.edu>
2014-05-20 09:32:06 -0700

    Removes use of named pipes

--- fake_data/fakeit.sh
+++ fake_data/fakeit.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+#Problem: this brakes the mkfifo procedure!!
+
 PATH2PLINK=../../plink_test/plink-1.07-srcKRT
 
 #Simulate some data.
-ms 12000 1 -s 500 | ./ms2plink fake.ped fake.map
+ms 12000 1 -s 500 | ./ms2plink testdata.ped testdata.map
 #Make binary input files
-$PATH2PLINK/plink --noweb --file fake --make-bed --map3 --out fake
+$PATH2PLINK/plink --noweb --file testdata --make-bed --map3 --out testdata
 #Do perms
-$PATH2PLINK/plink --noweb --bfile fake --assoc --map3 --mperm 1000000 --mperm-save-all --hwe 1e-6 --out fake &
-gzip fake.mperm.dump.all
+$PATH2PLINK/plink --noweb --bfile testdata --assoc --map3 --mperm 100000 --mperm-save-all --hwe 1e-6 --out testdata 
+
