500c476a8c1253ecc82a5825837d054e8aeca378
Kevin R. THORNTON <krthornt@hpc-login-1-2.local>
2014-06-13 15:46:34 -0700

    beginning of loop through data

--- src/esmk.cc
+++ src/esmk.cc
@@ -28,12 +28,13 @@ using namespace H5;
 struct esm_options
 {
   string outfile;
-  unsigned winsize,jumpsize,K;
+  int winsize,jumpsize,K;
   vector<string> infiles;
 };
 
 esm_options parseargs( int argc, char ** argv );
 bool permfilesOK( const esm_options & O );
+void run_test( const esm_options & O );
 
 int main( int argc, char ** argv )
 {
@@ -43,6 +44,9 @@ int main( int argc, char ** argv )
       cerr << "Error with permutation files\n";
       exit(10);
     }
+  run_test(O);
+
+  exit(0);
 }
 
 esm_options parseargs( int argc, char ** argv )
@@ -53,9 +57,9 @@ esm_options parseargs( int argc, char ** argv )
   desc.add_options()
     ("help,h", "Produce help message")
     ("outfile,o",value<string>(&rv.outfile),"Output file name.  Format is gzipped")
-    ("winsize,w",value<unsigned>(&rv.winsize),"Window size (bp)")
-    ("jumpsize,j",value<unsigned>(&rv.jumpsize),"Window jump size (bp)")
-    ("K,k",value<unsigned>(&rv.K),"Number of markers to use for ESM_k stat in a window")
+    ("winsize,w",value<int>(&rv.winsize),"Window size (bp)")
+    ("jumpsize,j",value<int>(&rv.jumpsize),"Window jump size (bp)")
+    ("K,k",value<int>(&rv.K),"Number of markers to use for ESM_k stat in a window.  Must be > 0.")
     ;
 
   variables_map vm;
@@ -118,4 +122,58 @@ bool permfilesOK( const esm_options & O )
     }
 
   return true;
+}
+
+struct within
+{
+  typedef bool result_type;
+  inline bool operator()(const int & val,
+			 const int & left,
+			 const int & right) const
+  {
+    return (val >= left && val <= right);
+  }
+};
+pair<size_t,size_t> get_indexes( const vector<int> & pos,
+				 const int & left,
+				 const int & right )
+{
+  vector<int>::const_iterator ci1 = find_if(pos.begin(),pos.end(),
+					    boost::bind( within(),_1,left,right));
+  vector<int>::const_reverse_iterator ci2 = find_if(pos.rbegin(),pos.rend(),
+						    boost::bind(within(),_1,left,right) );
+
+  if( ci1 == pos.end() && ci2 == pos.rend() )
+    {
+      //no snps are in window
+      return make_pair( numeric_limits<size_t>::max(),
+			numeric_limits<size_t>::max() );
+    }
+  cerr << *ci1 << ' ' << *ci2 << '\n';
+  return make_pair( ci1-pos.begin(), pos.rend()-ci2-1 );
+}
+				 
+void run_test( const esm_options & O )
+{
+  vector<string> chroms_0 = read_strings(O.infiles[0].c_str(),"/Markers/chr");
+  set<string> sc_0(chroms_0.begin(),chroms_0.end());
+  chroms_0.clear();
+  vector<string> markers_0 = read_strings(O.infiles[0].c_str(),"/Markers/IDs");
+  vector<int> pos_0 = read_ints(O.infiles[0].c_str(),"/Markers/pos");  
+
+  int left = 1, right = left + O.winsize - 1;
+
+  unsigned j=0;
+  const int LPOS = *(pos_0.end()-1);
+  do
+    {
+      pair<size_t,size_t> indexes = get_indexes( pos_0, left,right );
+      left += O.jumpsize;
+      right += O.jumpsize;
+      if( indexes.first != numeric_limits<size_t>::max() )
+	{
+	  cerr << left << ' ' << right << ' ' << pos_0[indexes.first] << ' ' << pos_0[indexes.second] << '\n';
+	}
+    }
+  while(left <= LPOS);  //not a great way to terminate.  Look @ libseq for guidance
 }
