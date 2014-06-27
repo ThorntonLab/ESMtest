c6dd572f12a9f050b4e1957664505c6ed431836a
Jaleal Sanjak <jsanjak@compute-1-13.local>
2014-06-27 15:05:46 -0700

    seems to work, see TODO list

--- src/esmk.cc
+++ src/esmk.cc
@@ -1,5 +1,11 @@
-//HPC modules needed: hdf5/1.8.11 boost/1.54.0 zlib/1.2.7
-
+ //HPC modules needed: hdf5/1.8.11 boost/1.54.0 zlib/1.2.7
+/*
+TODO:
+1)determine a method to deal with multiple chromosomes
+2)determine best output format
+3)test effect of window size, jump size and number of markers
+4)deal with LD
+ */
 /*
   libhdf5 -- the C++ interface is in this header.
 
@@ -19,6 +25,7 @@
 //Standard c++
 #include <iostream>   //let's us print to screen
 #include <string>     //strings = containers of characters
+#include <fstream>    //let's us write to files
 #include <vector>     //vectors = containers of other things
 #include <cmath>      //The C++ version of C's math.h (puts the C functions in namespace std)
 #include <algorithm>  //find, sort, etc.
@@ -154,7 +161,7 @@ bool permfilesOK( const esm_options & O )
 
   for ( size_t i = 1 ; i < O.infiles.size() ; ++i )
     {
-      vector<string> chroms_i = read_strings(O.infiles[0].c_str(),"/Markers/chr");
+      vector<string> chroms_i = read_strings(O.infiles[i].c_str(),"/Markers/chr");
       set<string> sc_i(chroms_i.begin(),chroms_i.end());
       if( sc_i.size() > 1 ) { return false; }
       vector<string> markers_i = read_strings(O.infiles[i].c_str(),"/Markers/IDs");
@@ -195,55 +202,144 @@ void run_test( const esm_options & O )
   chroms_0.clear();
   //1b: the rsID for the markers
   vector<string> markers_0 = read_strings(O.infiles[0].c_str(),"/Markers/IDs");
+  
+  //get the observed chisqs:
+  vector<double> chisq_obs = read_doubles(O.infiles[0].c_str(),"/Perms/observed");
+  
   //1c: the marker positions
   vector<int> pos_0 = read_ints(O.infiles[0].c_str(),"/Markers/pos");  
-
+  
+ 
   //Step 2: establish the left and right boundaries of the first window
   int left = 1, right = left + O.winsize - 1;
 
   //Step 3: go over the current data and make sure that our helper functions are working
-  unsigned j=0; 
-  const int LPOS = *(pos_0.end()-1); //This is the last position in pos_0.  Equivalent to pos[pos.size()-1], but I guess I like to complicate things.
+  //unsigned j=0; 
+   const int LPOS = *(pos_0.end()-1); //This is the last position in pos_0.  Equivalent to pos[pos.size()-1], but I guess I like to complicate things.
+  
+  //declare vectors for the final PVALUES, the midpoint of associated window and chromosome(dumbway):
+  vector<double> p_values;
+  vector<double> midpoints;
+
+  // vector<double> chrom_track;
+  
   do
     {
       //3a: get the indexes in pos_0 corresponding to left- and right- most SNPs in this window
       pair<size_t,size_t> indexes = get_indexes( pos_0, left,right );
       //3b: count number of markers in this window
       size_t nmarkers = indexes.second - indexes.first + 1;
+      size_t markers_used = O.K;
       if( indexes.first != numeric_limits<size_t>::max() ) //If there are SNPs in the window
 	{
-	  /*
-	    Skip windows w/no markers.
-
-	    This is where modifications need to be made. 
-
-	    The permutation files all need to be processed here, and then the test statistic calculated for each window.
-
-	    For the moment, forget about LD filtering.
-
-	    I believe that the loop will look something like this:
-	    vector<double> chisqs; //store permuted values of chisq in 1 big vector.
-
-	    for( size_t i = 0 ; i < O.infiles.size() ; ++ i )
+	  
+	  vector<double> ESM_perm;
+	  //for each file:
+	  for( size_t i = 0 ; i < O.infiles.size() ; ++i ) 
 	    {
-	    //in here, open the hdf5 file, figure out how many markers are in the 
-	    //interval from (indexes.first,indexes.second), and read them into chisqs.
+	      //Get a the slab in vector form from the file
+	      vector<double> newdata = read_doubles_slab(O.infiles[i].c_str(),"/Perms/permutations",indexes.first, nmarkers);
+	      
+	      //this should be the same, but may as well determine it here
+	      size_t nperms_file = newdata.size()/nmarkers;
+	      
+	      //prepare the vector of esm values  for the new values from the file
+	      //we have one ESM value for each perm
+	      ESM_perm.resize(ESM_perm.size() + nperms_file);
+	      
+	      //for each perm in the new data
+	      for ( size_t j = 0; j< nperms_file; ++j)
+		{
+		  //sort the markers for this range ( this sorts in ascending order)
+		  sort( newdata.begin()+ nmarkers*j,
+			newdata.begin() + nmarkers*j + nmarkers,
+			boost::bind(greater<double>(),_1,_2)
+			);
 
-	    //Useful things to know about std::vector<T>:
-	    //Declare an empty one:
-	    //vector<double> x;
-	    //resize to contain 1,000 doubles
-	    //x.resize( 1000 );
-	    //You'll need to pass the HDF5 library a pointer to where you want to read.
-	    //If that position is size_t POS, 
-	    //then passing &x[POS] passes an array of doubles (double * in C) to HDF5.
-	    //This array will be a sub-array of x, beginning at position POS+1.
+		  		  
+		  double ESM = 0;
+		  /*calculate the ESM
+		    Which is:
+		    
+		    ESM = SUM_k_M(Y_k + log10(k/M))
+		    
+		    where Y_k is the kth most significant chisq value and M is the number
+		    of markers considered.
+		    
+	     	   */
+		  // need to go from 0 to min(markers_used, nmarkers)
+		  for ( size_t k =0 ; k < min(markers_used,nmarkers) ; ++k )
+		    {
+
+		      ESM += newdata[nmarkers*j + k] + log10(((double) k + 1) / (double) nmarkers); 
+
+ 		    }
+		  //j = 0:n_perms -1, i = 0:nfiles-1
+		  ESM_perm[j + nperms_file*i] = ESM;
+		  
+		} 
+	      //probably unnecessary
+	      newdata.clear();
+	     		  		   		    
 	    }
-	  */
-	  cerr << left << ' ' << right << ' ' << pos_0[indexes.first] << ' ' << pos_0[indexes.second] << ' ' << nmarkers << '\n';
+
+	  //Now that we have the vector of ESM values we can calculate the P-value for the window
+	  
+	  //Calulate the observed ESM:
+	  //first copy the observed valuse into a vector to operate on
+	  //this is not the most efficient way, but was the easiest fix
+	  vector<double> chisq_win = chisq_obs; 
+	  sort( chisq_win.begin() + indexes.first, 
+		chisq_win.begin() + indexes.second,
+		boost::bind(greater<double>(),_1,_2)
+		);
+
+	  double ESM_obs = 0;
+	  size_t n;    
+	  for ( size_t q = indexes.first;q < indexes.first + min(markers_used, nmarkers); ++q )
+	    {
+	     
+	      n  = q - indexes.first + 1;
+	      
+	      ESM_obs += chisq_win[q] + log10((double) n / (double) nmarkers);
+	      
+	    }
+	  	  
+	  /*Now calculate the windows' p-value as
+	   the number of (ESM_perms >= ESM_obs)/ #Perms
+	   */
+	  double mid = 0.5*(right + left); // the window mid point
+	  midpoints.push_back(mid);
+	  
+	  double PVAL_win = 0 ;
+	  for ( size_t i = 0; i < ESM_perm.size(); ++i)//for each perm
+	    {
+	      if (ESM_perm[i]>=ESM_obs)//if the ESM is larger than observed
+		{
+		  PVAL_win += 1 ;
+		}
+	    }
+	  
+	  PVAL_win /= (double)ESM_perm.size();//divide by number of perms
+	  p_values.push_back(PVAL_win);
+	 
+	  //not tracking chromosomes yet
+	  // chroms_track.push_back(chr);
 	}
-       left += O.jumpsize;
+      left += O.jumpsize;
       right += O.jumpsize;
     }
-  while(left <= LPOS);  //Possibly not a great way to terminate.  Look @ libseq for guidance
+  while(right <= LPOS);  //Possibly not a great way to terminate.  Look @ libseq for guidance
+  
+ 
+  ofstream output;
+  output.open(O.outfile.c_str());
+  output << "p.values"<<' '<<"loci.midpoint"<<'\n';
+  for ( size_t i = 0; i< p_values.size(); ++i)
+    { 
+      
+      output<<p_values[i]<<' '<<midpoints[i]<<'\n';//<<chroms_track[i]<<'\n';
+    }
+   output.close();
+  
 }
