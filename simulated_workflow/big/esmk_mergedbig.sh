a9b082dc0739f35dd54d69e9ef1293065a7ffc09
Jaleal Sanjak <jsanjak@uci.edu>
2015-09-22 14:50:43 -0700

    updated examples

--- simulated_workflow/big/esmk_mergedbig.sh
+++ simulated_workflow/big/esmk_mergedbig.sh
@@ -18,4 +18,4 @@ module load krthornt/thorntonlab/1.0
 cd $SGE_O_WORKDIR
 
 #Process permutations in chunks of 50 records at a time                                                                                                           
-/data/users/jsanjak/ESMtest/src/esmk -o bigfake_merged.esmpv.txt -w 10000 -j 1000 -k 50 -n $CORES -r 0.5 --cmarkers 50 --cperms 10000 --nperms 100000 bigfake_merged.all.perms.h5
+esmk -o bigfake_merged.esmpv.txt -w 10000 -j 1000 -k 50 -n $CORES -r 0.5 --cmarkers 50 --cperms 10000 --nperms 100000 bigfake_merged.all.perms.h5
