95e0705114ba9a4bc803fdbdb1819a6ca28da615
Jaleal Sanjak <jsanjak@uci.edu>
2015-09-22 15:37:03 -0700

    doc examples

--- simulated_workflow/small/permute.sh
+++ simulated_workflow/small/permute.sh
@@ -1,8 +1,20 @@
 #!/bin/sh
+#On UCI HPC we use                
+#module purge
+#module load krthornt/thorntonlab/1.0
+#WHICH LOADS:
+##gcc/4.8.4
+##gsl/1.16
+##htslib/1.2.1
+##zlib/1.2.8
+##boost/1.59.0
+##hdf5/1.8.15-patch1
+##python/2.7.10
+##krthornt/libsequence/1.8.5
 
-module purge
-module load krthornt/thorntonlab/1.0
-module load plink/1.90a
+#module load plink/1.90a
+
+#Modify as needed for your system                                                                                                                 
 #NEED:
 #zlib
 #boost
