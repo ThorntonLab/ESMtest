1dd6bf50c7b62a44a344e83ade6c61ec08509fc5
Jaleal Sanjak <jsanjak@uci.edu>
2015-09-25 13:03:02 -0700

    updated examples

--- simulated_workflow/submit.sh
+++ simulated_workflow/submit.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-#Make the data
-sh makedata.sh
 #Permute the data
 sh permute.sh 
 #Run the ESM test
@@ -8,7 +6,6 @@ sh esmk.sh
 
 
 #OPTION TO MERGE THE H5FILES
-#sh makedata.sh
 #sh permute.sh
 #sh merge.sh
 #sh esmk_merged.sh
